7abb3e04d8233cc010600c78da9df82e91ffd7f5
Adithya Kamath <adithyakamath96@gmail.com>
2020-12-20 03:36:37 +0530

    Bug still not squished. App interface ready

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import firebase from "firebase/app";
 import "firebase/auth";
+import firebase from 'firebase/app';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_REACT_APP_AUTH_DOMAIN,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
+console.log(app.apiKey);
 
 export const auth = app.auth();
 export default app;
